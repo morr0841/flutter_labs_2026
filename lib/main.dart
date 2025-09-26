e92f2e18c1d60be7cafecb4f873717e67d9b818a
Elairis Morrow <morr0841@algonquinlive.com>
2025-09-26 13:24:53 -0400

    Added Images to images folder.

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, //this hides the debug banner
+      title: 'Lab 03',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Lab 03'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
 
   final String title;
 
@@ -30,44 +33,126 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _counter = 0;
-  void setNewValue(double value)
-  {
-    _counter = value;
-  }
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+        return Scaffold(   //for the whole page
 
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:', ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Slider(value: _counter, onChanged: setNewValue,)
-          ],
-        ),
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text("BROWSE CATEGORIES", style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+              Padding(padding: EdgeInsets.all(26.0),
+              child:
+                Text("Not sure what you're looking for? Do a search or dive into our most popular categories.",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text("BY MEAT", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                   Stack( alignment: AlignmentDirectional.center,
+                     children: <Widget>[
+                     CircleAvatar(
+                      backgroundImage: AssetImage("images/beef.jpg"), radius: 60),
+                       Text("BEEF", style: TextStyle(fontSize: 30.0, color: Colors.white),)
+                     ]),
+                    Stack( alignment: AlignmentDirectional.center,
+                        children: <Widget>[
+                          CircleAvatar(
+                              backgroundImage: AssetImage("images/chicken.jpg"), radius: 60),
+                          Text("CHICKEN", style: TextStyle(fontSize: 30.0, color: Colors.white),)
+                        ]),
+                    Stack( alignment: AlignmentDirectional.center,
+                        children: <Widget>[
+                          CircleAvatar(
+                              backgroundImage: AssetImage("images/pork.jpg"), radius: 60),
+                          Text("PORK", style: TextStyle(fontSize: 30.0, color: Colors.white),)
+                        ]),
+                    Stack( alignment: AlignmentDirectional.center,
+                        children: <Widget>[
+                          CircleAvatar(
+                              backgroundImage: AssetImage("images/seafood.jpeg"), radius: 60),
+                          Text("SEAFOOD", style: TextStyle(fontSize: 30.0, color: Colors.white),)
+                        ]),
+                      ],
+                  ),
+              Text("BY COURSE", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Stack( alignment: AlignmentDirectional.bottomCenter,
+                      children: <Widget>[
+                        CircleAvatar(
+                            backgroundImage: AssetImage("images/main-dishes.jpg"), radius: 60),
+                        Text("Main Dishes", style: TextStyle(fontSize: 30.0, color: Colors.black, backgroundColor: Color.fromRGBO(255, 255, 255, 0.3)),)
+                      ]),
+                  Stack( alignment: AlignmentDirectional.bottomCenter,
+                      children: <Widget>[
+                        CircleAvatar(
+                            backgroundImage: AssetImage("images/salad.jpeg"), radius: 60),
+                        Text("Salad Recipes", style: TextStyle(fontSize: 30.0, color: Colors.black, backgroundColor: Color.fromRGBO(255, 255, 255, 0.3)),)
+                      ]),
+                  Stack( alignment: AlignmentDirectional.bottomCenter,
+                      children: <Widget>[
+                        CircleAvatar(
+                            backgroundImage: AssetImage("images/side-dishes.jpeg"), radius: 60),
+                        Text("Side Dishes", style: TextStyle(fontSize: 30.0, color: Colors.black, backgroundColor: Color.fromRGBO(255, 255, 255, 0.3)),)
+                      ]),
+                  Stack( alignment: AlignmentDirectional.bottomCenter,
+                      children: <Widget>[
+                        CircleAvatar(
+                            backgroundImage: AssetImage("images/crockpot.jpg"), radius: 60),
+                        Text("Crockpot", style: TextStyle(fontSize: 30.0, color: Colors.black, backgroundColor: Color.fromRGBO(255, 255, 255, 0.3)),)
+                      ]),
+                ],
+              ),
+              Text("BY DESSERT", style: TextStyle(backgroundColor: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Stack( alignment: AlignmentDirectional.bottomCenter,
+                      children: <Widget>[
+                        CircleAvatar(
+                            backgroundImage: AssetImage("images/icecream.jpeg"), radius: 60),
+                        Text("Ice Cream", style: TextStyle(fontSize: 30.0, color: Colors.black, backgroundColor: Color.fromRGBO(255, 255, 255, 0.3)),)
+                      ]),
+                  Stack( alignment: AlignmentDirectional.bottomCenter,
+                      children: <Widget>[
+                        CircleAvatar(
+                            backgroundImage: AssetImage("images/brownies.jpeg"), radius: 60),
+                        Text("Brownies", style: TextStyle(fontSize: 30.0, color: Colors.black, backgroundColor: Color.fromRGBO(255, 255, 255, 0.3)),)
+                      ]),
+                  Stack( alignment: AlignmentDirectional.bottomCenter,
+                      children: <Widget>[
+                        CircleAvatar(
+                            backgroundImage: AssetImage("images/pies.jpeg"), radius: 60),
+                        Text("Pies", style: TextStyle(fontSize: 30.0, color: Colors.black, backgroundColor: Color.fromRGBO(255, 255, 255, 0.3)),)
+                      ]),
+                  Stack( alignment: AlignmentDirectional.bottomCenter,
+                      children: <Widget>[
+                        CircleAvatar(
+                            backgroundImage: AssetImage("images/cookies.jpg"), radius: 60),
+                        Text("Cookies", style: TextStyle(fontSize: 30.0, color: Colors.black, backgroundColor: Color.fromRGBO(255, 255, 255, 0.3)),)
+                      ]),
+                ],
+              ),
+            ],
+          ),
+        )
     );
+  }
+
+  void buttonClicked()
+  {
+
   }
 }
